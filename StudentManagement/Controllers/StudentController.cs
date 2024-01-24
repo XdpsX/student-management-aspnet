eb00e3e91fcf4fe2b4ae93474ab98eeef6a276fe
Night <dranhata@gmail.com>
2024-01-24 22:00:55 +0700

    first commit

---
+++ StudentManagement/Controllers/StudentController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagement.Dtos;
+using StudentManagement.Entities;
+using System.Xml.Linq;
+
+namespace StudentManagement.Controllers
+{
+    [ApiController]
+    [Route("api/v1/students")]
+    public class StudentController : Controller
+    {
+        private static int _id = 0;
+        private static List<Student> _students = new List<Student>();
+
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            return Ok(_students);
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent([FromBody] StudentRequest input)
+        {
+            Student student = new Student();
+            student.Id = _id++;
+            student.Name = input.StudentName;
+            student.StudentCode = input.StudentCode;
+            student.DateOfBirth = input.DateOfBirth;
+            _students.Add(student);
+
+            return Ok(student); 
+        }
+
+        [HttpGet("path-id/{id}")]
+        public IActionResult getStudentByIdInPath(int id)
+        {
+            var student = _students.Find(s => s.Id == id);
+            if (student == null)
+                return NotFound();
+
+            return Ok(student);
+        }
+
+        [HttpGet("param-id")]
+        public IActionResult getStudentByIdInParam([FromQuery] int id)
+        {
+            var student = _students.Find(s => s.Id == id);
+            if (student == null)
+                return NotFound();
+
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult updateStudentById(int id, [FromBody] StudentRequest student)
+        {
+            var existingStudent = _students.Find(s => s.Id == id);
+            if (existingStudent == null)
+                return NotFound();
+
+            existingStudent.Name = student.StudentName;
+            existingStudent.StudentCode = student.StudentCode;
+            existingStudent.DateOfBirth = student.DateOfBirth;
+
+            return Ok(existingStudent);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudentById(int id)
+        {
+            var sinhVien = _students.Find(sv => sv.Id == id);
+            if (sinhVien == null)
+                return NotFound();
+
+            _students.Remove(sinhVien);
+            return Ok();
+        }
+    }
+}
---
+++ StudentManagement/Dtos/StudentRequest.cs
@@ -0,0 +1,36 @@
+﻿namespace StudentManagement.Dtos
+{
+    public class StudentRequest
+    {
+        private string _studentName;
+        public string StudentName
+        {
+            get => _studentName;
+            set => _studentName = value?.Trim();
+        }
+
+        private string _studentCode;
+        public string StudentCode
+        {
+            get => _studentCode;
+            set => _studentCode = value?.Trim();
+        }
+
+        private DateTime _dateOfBirth;
+        public DateTime DateOfBirth
+        {
+            get => _dateOfBirth;
+            set
+            {
+                if (value < DateTime.Now)
+                {
+                    _dateOfBirth = value;
+                }
+                else
+                {
+                    throw new ArgumentException("BirthOfDate must be before the current date.");
+                }
+            }
+        }
+    }
+}
